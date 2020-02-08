67b238de20ce22bf2da9f683bee099d3f431b5a9
Joe Greener <jgreener@hotmail.co.uk>
2020-02-08 22:24:59 +0000

    more benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,6 +4,7 @@
 using BioStructures
 using BioCore
 using BenchmarkTools
+using Random
 
 # Use files in BioFmtSpecimens
 fmtdir = BioCore.Testing.get_bio_fmt_specimens()
@@ -22,6 +23,7 @@ const SUITE = BenchmarkGroup(
         "read"   => BenchmarkGroup([], [f=> BenchmarkGroup() for f in keys(formats)]...),
         "write"  => BenchmarkGroup([], [f=> BenchmarkGroup() for f in keys(formats)]...),
         "dict"   => BenchmarkGroup(),
+        "model"  => BenchmarkGroup(),
         "collect"=> BenchmarkGroup(),
         "spatial"=> BenchmarkGroup(),
 )
@@ -36,24 +38,36 @@ for pdbid in pdbids
         SUITE["read"][f][pdbid] = @benchmarkable read(
                 $(testfilepath(f, "$pdbid.$(pdbextension[formats[f]])")), $(formats[f]))
         SUITE["write"][f][pdbid] = @benchmarkable $(writefunctions[f])(
-                $temp_filename, $(struc[pdbid])) teardown=(rm(temp_filename))
+                $temp_filename, $(struc[pdbid])) teardown=(rm(temp_filename, force=true))
     end
 end
 
 SUITE["dict"]["mmCIF"] = @benchmarkable MMCIFDict($(testfilepath("mmCIF", "1AKE.cif" )))
 SUITE["dict"]["MMTF" ] = @benchmarkable MMTFDict( $(testfilepath("MMTF" , "1AKE.mmtf")))
 
-SUITE["collect"]["atoms"      ] = @benchmarkable collectatoms(   $(struc["1EN2"]))
-SUITE["collect"]["atomssel"   ] = @benchmarkable collectatoms(   $(struc["1EN2"]), calphaselector)
-SUITE["collect"]["atomsdis"   ] = @benchmarkable collectatoms(   $(struc["1EN2"]), expand_disordered=true)
-SUITE["collect"]["residues"   ] = @benchmarkable collectresidues($(struc["1EN2"]))
-SUITE["collect"]["residuessel"] = @benchmarkable collectresidues($(struc["1EN2"]), standardselector)
-SUITE["collect"]["residuesdis"] = @benchmarkable collectresidues($(struc["1EN2"]), expand_disordered=true)
-SUITE["collect"]["chains"     ] = @benchmarkable collectchains(  $(struc["1AKE"]))
-SUITE["collect"]["models"     ] = @benchmarkable collectmodels(  $(struc["1SSU"]))
+SUITE["model"]["atomaccess"  ] = @benchmarkable $(struc["1EN2"])["A"][20]["CA"]
+SUITE["model"]["sortatoms"   ] = @benchmarkable sort(ats) setup=(ats = shuffle(collectatoms(   struc["1EN2"])))
+SUITE["model"]["sortresidues"] = @benchmarkable sort(res) setup=(res = shuffle(collectresidues(struc["1EN2"])))
+SUITE["model"]["iterate"     ] = @benchmarkable for mod in $(struc["1AKE"]) for ch in mod for res in ch for at in res end end end end
+SUITE["model"]["sequence"    ] = @benchmarkable AminoAcidSequence($(collectresidues(struc["1EN2"])))
+SUITE["model"]["pdbline"     ] = @benchmarkable pdbline($(struc["1EN2"]["A"][20]["CA"]))
 
-SUITE["spatial"]["coordarray"  ] = @benchmarkable coordarray($(collectatoms(struc["1AKE"])))
-SUITE["spatial"]["distance"    ] = @benchmarkable distance($(struc["1AKE"]["A"][50]), $(struc["1AKE"]["A"][60]))
-SUITE["spatial"]["ramachandran"] = @benchmarkable ramachandranangles($(struc["1AKE"]["A"]), standardselector)
-SUITE["spatial"]["contactmap"  ] = @benchmarkable ContactMap($(collectatoms(struc["1AKE"]["A"], cbetaselector)), 8.0)
-SUITE["spatial"]["distancemap" ] = @benchmarkable DistanceMap($(collectresidues(struc["1AKE"]["A"], standardselector)))
+SUITE["collect"]["atoms"       ] = @benchmarkable collectatoms(   $(struc["1EN2"]))
+SUITE["collect"]["atomssel"    ] = @benchmarkable collectatoms(   $(struc["1EN2"]), calphaselector)
+SUITE["collect"]["atomsdis"    ] = @benchmarkable collectatoms(   $(struc["1EN2"]), expand_disordered=true)
+SUITE["collect"]["residues"    ] = @benchmarkable collectresidues($(struc["1EN2"]))
+SUITE["collect"]["residuessel" ] = @benchmarkable collectresidues($(struc["1EN2"]), standardselector)
+SUITE["collect"]["residuesdis" ] = @benchmarkable collectresidues($(struc["1EN2"]), expand_disordered=true)
+SUITE["collect"]["chains"      ] = @benchmarkable collectchains(  $(struc["1AKE"]))
+SUITE["collect"]["models"      ] = @benchmarkable collectmodels(  $(struc["1SSU"]))
+
+SUITE["spatial"]["coordarray"    ] = @benchmarkable coordarray($(collectatoms(struc["1AKE"])))
+SUITE["spatial"]["transformation"] = @benchmarkable Transformation($(collectresidues(struc["1SSU"][5], standardselector)), $(collectresidues(struc["1SSU"][10], standardselector)))
+SUITE["spatial"]["rmsd"          ] = @benchmarkable rmsd($(struc["1SSU"][5]), $(struc["1SSU"][10]), superimpose=false, rmsdatoms=heavyatomselector)
+SUITE["spatial"]["distance"      ] = @benchmarkable distance($(struc["1AKE"]["A"][50]), $(struc["1AKE"]["A"][60]))
+SUITE["spatial"]["bondangle"     ] = @benchmarkable bondangle($(struc["1AKE"]["A"][20]["N"]), $(struc["1AKE"]["A"][20]["CA"]), $(struc["1AKE"]["A"][20]["C"]))
+SUITE["spatial"]["dihedralangle" ] = @benchmarkable dihedralangle($(struc["1AKE"]["A"][20]["N"]), $(struc["1AKE"]["A"][20]["CA"]), $(struc["1AKE"]["A"][20]["C"]), $(struc["1AKE"]["A"][21]["N"]))
+SUITE["spatial"]["ramachandran"  ] = @benchmarkable ramachandranangles($(struc["1AKE"]["A"]), standardselector)
+SUITE["spatial"]["contactmap"    ] = @benchmarkable ContactMap($(collectatoms(struc["1AKE"]["A"], cbetaselector)), 8.0)
+SUITE["spatial"]["distancemap"   ] = @benchmarkable DistanceMap($(collectresidues(struc["1AKE"]["A"], standardselector)))
+SUITE["spatial"]["contactgraph"  ] = @benchmarkable MetaGraph($(collectatoms(struc["1AKE"]["A"], cbetaselector)), 8.0)
