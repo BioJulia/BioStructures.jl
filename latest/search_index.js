f01baabfae1f61a16719e0e38872aab684e10376
autodocs <autodocs>
2020-04-13 10:48:44 +0000

    build based on b8cc531

--- latest/search_index.js
+++ latest/search_index.js
@@ -165,7 +165,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "BioSequences.AminoAcidSequence",
     "category": "method",
-    "text": "AminoAcidSequence(el)\n\nReturn the amino acid sequence of a protein.\n\nAdditional arguments are residue selector functions - only residues that return true from all the functions are retained. The gaps keyword argument determines whether to add gaps to the sequence based on missing residue numbers (default true). See BioSequences.jl for more on how to use sequences.\n\n\n\n\n\n"
+    "text": "AminoAcidSequence(el)\n\nReturn the amino acid sequence of a structural element.\n\nAdditional arguments are residue selector functions - only residues that return true from all the functions are retained. The gaps keyword argument determines whether to add gaps to the sequence based on missing residue numbers (default true). See BioSequences.jl for more on how to use sequences.\n\n\n\n\n\n"
 },
 
 {
