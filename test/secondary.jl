905e9ab264b1888834adacf93830ff3ad241b782
Shu Li <zetarylee@gmail.com>
2023-09-19 01:11:16 +0800

    Add tests for secondary.jl

--- test/secondary.jl
+++ test/secondary.jl
@@ -1,7 +1,41 @@
 using BioStructures
 using Test
 
-@testset "Secondary Structure Prediction" begin
+@testset "Secondary Structure Information" begin
+    @testset "Residue Manipulation 1" begin
+        res = Residue("ALA", 1, ' ', false, Chain('A'))
+        ss_code!(res, "H")
+        @test ss_code(res) == "H"
+
+        res = Residue(
+            "ALA",
+            1,
+            ' ',
+            false,
+            ["CA"],
+            Dict(
+                "CA" => Atom(
+                    1,
+                    "CA",
+                    ' ',
+                    [0.0, 0.0, 0.0],
+                    1.0,
+                    0.0,
+                    "  ",
+                    "  ",
+                    Residue("ALA", 1, ' ', false, Chain('A')),
+                ),
+            ),
+            Chain('A'),
+            "T"
+        )
+        @test ss_code(res) == "T"
+
+        for at in atoms(res)
+            @test ss_code(at) == "T"
+        end
+    end
+
     @testset "DSSP" begin
         pdb_path = downloadpdb("1BQ0", dir = tempname() * ".cif", format = MMCIF)
         struc = read(pdb_path, MMCIF)
