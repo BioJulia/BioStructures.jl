5e388d7ce592aceada6634dfce531332b9329675
Joe Greener <jgreener@hotmail.co.uk>
2023-10-01 14:06:04 +0100

    docs fix page size error

--- docs/make.jl
+++ docs/make.jl
@@ -2,13 +2,14 @@ using Documenter
 using BioStructures
 
 makedocs(
-    sitename = "BioStructures.jl",
-    pages = [
-        "Home"         => "index.md",
-        "Documentation"=> "documentation.md",
-        "Examples"     => "examples.md",
-        "API"          => "api.md",
-    ]
+    sitename="BioStructures.jl",
+    pages=[
+        "Home"          => "index.md",
+        "Documentation" => "documentation.md",
+        "Examples"      => "examples.md",
+        "API"           => "api.md",
+    ],
+    format=Documenter.HTML(size_threshold_ignore=["documentation.md"]),
 )
 
 deploydocs(
