3282b7714f13c6cad15f1fc12a6c0ce452f840ab
Leandro Martinez <lmartine@unicamp.br>
2024-05-27 14:45:12 -0300

    all keywords and macrokeywords working

--- src/select.jl
+++ src/select.jl
@@ -4,6 +4,75 @@
 #
 export select, Select, @sel_str
 
+#
+# Dictionaries of residue, atom, and element types, as originally written
+# in PDBTools.jl (at first sight, seems more general that the current)
+#
+#
+# Data for natural protein residues
+#
+Base.@kwdef struct ProteinResidue
+    name::String
+    three_letter_code::String
+    one_letter_code::String
+    type::String
+    polar::Bool
+    hydrophobic::Bool
+    mono_isotopic_mass::Float64
+    mass::Float64
+    charge::Int
+    custom::Bool = true
+end
+#! format: off
+const protein_residues = Dict{String,ProteinResidue}(
+    "ALA" => ProteinResidue("Alanine",       "ALA", "A", "Aliphatic",  false, false,  71.037114,  71.0779,  0, false),
+    "ARG" => ProteinResidue("Arginine",      "ARG", "R", "Basic",      true,  false, 156.101111, 156.1857,  1, false),
+    "ASN" => ProteinResidue("Asparagine",    "ASN", "N", "Amide",      true,  false, 114.042927, 114.1026,  0, false),
+    "ASP" => ProteinResidue("Aspartic acid", "ASP", "D", "Acidic",     true,  false, 115.026943, 115.0874, -1, false),
+    "CYS" => ProteinResidue("Cysteine",      "CYS", "C", "Sulfuric",   false, false, 103.009185, 103.1429,  0, false),
+    "GLN" => ProteinResidue("Glutamine",     "GLN", "Q", "Amide",      true,  false, 128.058578, 128.1292,  0, false),
+    "GLU" => ProteinResidue("Glutamic acid", "GLU", "E", "Acidic",     true,  false, 129.042593, 129.1140, -1, false),
+    "GLY" => ProteinResidue("Glycine",       "GLY", "G", "Aliphatic",  false, false,  57.021464,  57.0513,  0, false),
+    "HIS" => ProteinResidue("Histidine",     "HIS", "H", "Aromatic",   true,  false, 137.058912, 137.1393,  0, false), 
+    "ILE" => ProteinResidue("Isoleucine",    "ILE", "I", "Aliphatic",  false, true,  113.084064, 113.1576,  0, false),
+    "LEU" => ProteinResidue("Leucine",       "LEU", "L", "Aliphatic",  false, true,  113.084064, 113.1576,  0, false),
+    "LYS" => ProteinResidue("Lysine",        "LYS", "K", "Basic",      true,  false, 128.094963, 128.1723,  1, false),
+    "MET" => ProteinResidue("Methionine",    "MET", "M", "Sulfuric",   false, false, 131.040485, 131.1961,  0, false),
+    "PHE" => ProteinResidue("Phenylalanine", "PHE", "F", "Aromatic",   false, true,  147.068414, 147.1739,  0, false),
+    "PRO" => ProteinResidue("Proline",       "PRO", "P", "Cyclic",     false, false,  97.052764,  97.1152,  0, false),
+    "SER" => ProteinResidue("Serine",        "SER", "S", "Hydroxylic", true,  false,  87.032028, 87.07730,  0, false),
+    "THR" => ProteinResidue("Threonine",     "THR", "T", "Hydroxylic", true,  false, 101.047679, 101.1039,  0, false),
+    "TRP" => ProteinResidue("Tryptophan",    "TRP", "W", "Aromatic",   false, true,  186.079313, 186.2099,  0, false),
+    "TYR" => ProteinResidue("Tyrosine",      "TYR", "Y", "Aromatic",   true,  false, 163.063320, 163.1733,  0, false),
+    "VAL" => ProteinResidue("Valine",        "VAL", "V", "Aliphatic",  false, true,   99.068414,  99.1311,  0, false),
+    # Alternate protonation states for CHARMM and AMBER
+    "ASPP" => ProteinResidue("Aspartic acid (protonated)", "ASP", "D", "Acidic", true,  false, 115.026943, 115.0874, 0, false),
+    "GLUP" => ProteinResidue("Glutamic acid (protonated)", "GLU", "E", "Acidic", true,  false, 129.042593, 129.1140, 0, false),
+    "HSD"  => ProteinResidue("Histidine (D)", "HIS", "H", "Aromatic",   true,  false, 137.058912, 137.1393,  0, false), 
+    "HSE"  => ProteinResidue("Histidine (E)", "HIS", "H", "Aromatic",   true,  false, 137.058912, 137.1393,  0, false), 
+    "HSP"  => ProteinResidue("Histidine (doubly protonated)", "HIS", "H", "Aromatic",   true,  false, 137.058912, 137.1393,  1, false), 
+    "HID"  => ProteinResidue("Histidine (D)", "HIS", "H", "Aromatic",   true,  false, 137.058912, 137.1393,  0, false), 
+    "HIE"  => ProteinResidue("Histidine (E)", "HIS", "H", "Aromatic",   true,  false, 137.058912, 137.1393,  0, false), 
+    "HIP"  => ProteinResidue("Histidine (doubly protonated)", "HIS", "H", "Aromatic",   true,  false, 137.058912, 137.1393,  1, false), 
+)
+#! format: on
+isprotein(atom::AbstractAtom) = haskey(protein_residues, resname(atom))
+isacidic(atom::AbstractAtom) = isprotein(atom) && protein_residues[resname(atom)].type == "Acidic"
+isaliphatic(atom::AbstractAtom) = isprotein(atom) && protein_residues[resname(atom)].type == "Aliphatic"
+isaromatic(atom::AbstractAtom) = isprotein(atom) && protein_residues[resname(atom)].type == "Aromatic"
+isbasic(atom::AbstractAtom) = isprotein(atom) && protein_residues[resname(atom)].type == "Basic"
+ischarged(atom::AbstractAtom) = isprotein(atom) && protein_residues[resname(atom)].charge != 0
+isneutral(atom::AbstractAtom) = isprotein(atom) && protein_residues[resname(atom)].charge == 0
+ishydrophobic(atom::AbstractAtom) = isprotein(atom) && protein_residues[resname(atom)].hydrophobic
+ispolar(atom::AbstractAtom) = isprotein(atom) && protein_residues[resname(atom)].polar
+isnonpolar(atom::AbstractAtom) = isprotein(atom) && !ispolar(atom)
+const backbone_atoms = ["N", "CA", "C", "O"]
+isbackbone(atom::AbstractAtom; backbone_atoms=backbone_atoms) = isprotein(atom) && atomname(name) in backbone_atoms
+const not_side_chain_atoms = ["N", "CA", "C", "O", "HN", "H", "HA", "HT1", "HT2", "HT3"]
+issidechain(atom::AbstractAtom; not_side_chain_atoms=not_side_chain_atoms) = isprotein(atom) && !(atomname(atom) in not_side_chain_atoms)
+const water_residues = ["HOH", "OH2", "TIP3", "TIP3P", "TIP4P", "TIP5P", "TIP7P", "SPC", "SPCE"]
+iswater(atom::AbstractAtom) = resname(atom) in water_residues
+
 """
     Select
 
@@ -69,9 +138,6 @@ Base.findall(by::Select, struc::ProteinStructure) = serial.(atom_filter(by, stru
 # Function that returns true for all atoms: the default selection
 all(atoms) = true
 
-# Main function: receives the atoms vector and a julia function to select
-select(selection::String, struc) = atom_filter(Select(selection), struc)
-
 # Comparison operators
 const operators = (
     "=" => (x, y) -> isequal(x, y),
@@ -84,7 +150,6 @@ const operators = (
 #
 # Keywords
 #
-
 struct Keyword{F<:Function}
     ValueType::Type
     name::String
@@ -93,7 +158,7 @@ struct Keyword{F<:Function}
 end
 
 function (key::Keyword)(s::AbstractVector{<:AbstractString})
-    (;name, getter, operators) = key
+    (; name, getter, operators) = key
     for op in operators
         if (i = has_key(op.first, s)) > 0
             return el -> op.second(getter(el), parse_to_type(key, s[i+1]))
@@ -131,7 +196,7 @@ struct FunctionalKeyword{FunctionType}
 end
 
 function (key::FunctionalKeyword)(s::AbstractVector{<:AbstractString})
-    (;name, by, operators) = key
+    (; name, by, operators) = key
     for op in operators
         if (i = has_key(op.first, s)) > 0
             return el -> op.second(by(el), parse_to_type(key, s[i+1]))
@@ -175,45 +240,46 @@ function parse_to_type(key::Union{Keyword,FunctionalKeyword}, val)
 end
 
 #
-# Keywords for PDBTools
+# Keywords
 #
-
 keywords = [
     Keyword(Int, "serial", serial, operators),
     Keyword(Int, "resnumber", resnumber, operators),
+    Keyword(Int, "resnum", resnumber, operators),
     Keyword(Int, "resid", resid, operators),
     Keyword(Float64, "occupancy", occupancy, operators),
-    Keyword(Float64, "beta",  tempfactor, operators),
+    Keyword(Float64, "beta", tempfactor, operators),
     Keyword(Float64, "tempfactor", tempfactor, operators),
     Keyword(Int, "model", modelnumber, operators),
     Keyword(Int, "modelnumber", modelnumber, operators),
     Keyword(String, "name", atomname, operators),
     Keyword(String, "atomname", atomname, operators),
-#    Keyword(String, "segname", segname, operators),
+    #Keyword(String, "segname", segname, operators),
     Keyword(String, "resname", resname, operators),
     Keyword(String, "chain", chainid, operators),
     Keyword(String, "chainid", chainid, operators),
 ]
 
 macro_keywords = [
-    MacroKeyword("water", at -> resname(at) in waterresnames),
-#    MacroKeyword("water", at -> resname(at) in waterresnames),
-#    MacroKeyword("protein", at -> resname(at) in proteinresnames),
-#    MacroKeyword("polar", ispolar),
-#    MacroKeyword("nonpolar", isnonpolar),
-#    MacroKeyword("basic", isbasic),
-#    MacroKeyword("acidic", isacidic),
-#    MacroKeyword("charged", ischarged),
-#    MacroKeyword("aliphatic", isaliphatic),
-#    MacroKeyword("aromatic", isaromatic),
-#    MacroKeyword("hydrophobic", ishydrophobic),
-#    MacroKeyword("neutral", isneutral),
-#    MacroKeyword("backbone", isbackbone),
-#    MacroKeyword("sidechain", issidechain),
-#    MacroKeyword("all", all),
+    MacroKeyword("water", iswater),
+    MacroKeyword("protein", isprotein),
+    MacroKeyword("polar", ispolar),
+    MacroKeyword("nonpolar", isnonpolar),
+    MacroKeyword("basic", isbasic),
+    MacroKeyword("acidic", isacidic),
+    MacroKeyword("charged", ischarged),
+    MacroKeyword("aliphatic", isaliphatic),
+    MacroKeyword("aromatic", isaromatic),
+    MacroKeyword("hydrophobic", ishydrophobic),
+    MacroKeyword("neutral", isneutral),
+    MacroKeyword("backbone", isbackbone),
+    MacroKeyword("heavyatom", heavyatomselector),
+    MacroKeyword("disordered", isdisorderedatom),
+    MacroKeyword("sidechain", issidechain),
+    MacroKeyword("all", all),
 ]
 
-#functional_keywords = [FunctionalKeyword(String, "element", element, operators)]
+functional_keywords = [FunctionalKeyword(String, "element", element, operators)]
 
 #
 # parse_query and apply_query are a very gentle contribution given by 
@@ -396,3 +462,4 @@ parse_error(str) = throw(NoBackTraceException(ErrorException(str)))
 #    @test model.(eachresidue(lessresidues)) == [1, 1, 1, 1]
 #
 #end # testitem
+
