c0911d7f284934ad40631841514164cd83d9081c
Brady Johnston <brady.johnston@me.com>
2025-05-07 19:22:08 +0800

    run JuliaFormatter

--- src/bcif.jl
+++ src/bcif.jl
@@ -9,22 +9,22 @@ import MsgPack
 A function to read a binary CIF file from MolStar and extract the list of attributes and their compressed bytes.
 """
 function Base.read(input::IO,
-            ::Type{BCIFFormat},
-            structure_name::AbstractString="",
-            remove_disorder::Bool=false,
-            read_std_atoms::Bool=true,
-            read_het_atoms::Bool=true,
-            run_dssp::Bool=false,
-            run_stride::Bool=false)
-    
+    ::Type{BCIFFormat},
+    structure_name::AbstractString="",
+    remove_disorder::Bool=false,
+    read_std_atoms::Bool=true,
+    read_het_atoms::Bool=true,
+    run_dssp::Bool=false,
+    run_stride::Bool=false)
+
     file = MsgPack.unpack(read(input))
-    
+
     # currently just looking for the first data block
     categories = file["dataBlocks"][1]["categories"]
     atom_site = get_category(categories, "_atom_site")
     columns = atom_site[1]["columns"]
-    
-    attributes = Dict{String, Vector{Any}}()
+
+    attributes = Dict{String,Vector{Any}}()
     for column in columns
         attributes[column["name"]] = decode_column(column)
     end
@@ -33,11 +33,15 @@ end
 
 function get_category(cats::Vector{Any}, name::String)
     idx = findall(getindex.(cats, "name") .== name)
-    
-    if isnothing(idx) throw(ArgumentError("Category $name not found")) end
-    if length(idx) > 1 throw(ArgumentError("Multiple categories with name $name found")) end
 
-    return cats[idx] 
+    if isnothing(idx)
+        throw(ArgumentError("Category $name not found"))
+    end
+    if length(idx) > 1
+        throw(ArgumentError("Multiple categories with name $name found"))
+    end
+
+    return cats[idx]
 end
 
 # Enum for type codes
@@ -75,7 +79,7 @@ const INT_TO_TYPE = Dict(
     5 => UInt16,
     6 => UInt32,
     32 => Float32,
-    33 => Float64  
+    33 => Float64
 )
 
 
@@ -84,18 +88,18 @@ function safe_cast(array, dtype)
     if eltype(array) == dtype
         return array
     end
-    
+
     if dtype <: Integer && !(eltype(array) <: Integer)
         throw(ArgumentError("Cannot cast floating point to integer"))
     end
-    
+
     if dtype <: Integer
         type_min, type_max = typemin(dtype), typemax(dtype)
         if any(x -> x < type_min || x > type_max, array)
             throw(ArgumentError("Integer values do not fit into the given dtype"))
         end
     end
-    
+
     return convert(Array{dtype}, array)
 end
 
@@ -104,8 +108,8 @@ abstract type Encoding end
 
 # ByteArrayEncoding
 mutable struct ByteArrayEncoding <: Encoding
-    type::Union{TypeCode, Nothing}
-    
+    type::Union{TypeCode,Nothing}
+
     function ByteArrayEncoding(type=nothing)
         if type !== nothing
             type = type isa TypeCode ? type : TYPE_TO_TYPE_CODE[type]
@@ -129,7 +133,7 @@ end
 mutable struct FixedPointEncoding <: Encoding
     factor::Float64
     srcType::TypeCode
-    
+
     function FixedPointEncoding(factor; srcType=FLOAT32)
         srcType = srcType isa TypeCode ? srcType : TYPE_TO_TYPE_CODE[srcType]
         if !(srcType in (FLOAT32, FLOAT64))
@@ -153,7 +157,7 @@ mutable struct IntervalQuantizationEncoding <: Encoding
     max::Float64
     numSteps::Int
     srcType::TypeCode
-    
+
     function IntervalQuantizationEncoding(min, max, numSteps; srcType=FLOAT32)
         srcType = srcType isa TypeCode ? srcType : TYPE_TO_TYPE_CODE[srcType]
         new(min, max, numSteps, srcType)
@@ -177,9 +181,9 @@ end
 
 # RunLengthEncoding
 mutable struct RunLengthEncoding <: Encoding
-    srcSize::Union{Int, Nothing}
-    srcType::Union{TypeCode, Nothing}
-    
+    srcSize::Union{Int,Nothing}
+    srcType::Union{TypeCode,Nothing}
+
     function RunLengthEncoding(; srcSize=nothing, srcType=nothing)
         if srcType !== nothing
             srcType = srcType isa TypeCode ? srcType : TYPE_TO_TYPE_CODE[srcType]
@@ -197,13 +201,13 @@ function encode(enc::RunLengthEncoding, data)
     elseif enc.srcSize != length(data)
         throw(ArgumentError("Given source size does not match actual data size"))
     end
-    
+
     # Pessimistic allocation - worst case is run length of 1 for every element
     output = zeros(Int32, length(data) * 2)
     j = 1
     val = data[1]
     run_length = 0
-    
+
     for i in 1:length(data)
         curr_val = data[i]
         if curr_val == val
@@ -217,12 +221,12 @@ function encode(enc::RunLengthEncoding, data)
             run_length = 1
         end
     end
-    
+
     # Write last element
     output[j] = val
     output[j+1] = run_length
     j += 2
-    
+
     # Trim to correct size
     return output[1:j-1]
 end
@@ -231,7 +235,7 @@ function decode(enc::RunLengthEncoding, data)
     if length(data) % 2 != 0
         throw(ArgumentError("Invalid run-length encoded data"))
     end
-    
+
     length_output = 0
     if enc.srcSize === nothing
         # Determine length of output array by summing run lengths
@@ -241,25 +245,25 @@ function decode(enc::RunLengthEncoding, data)
     else
         length_output = enc.srcSize
     end
-    
+
     output = zeros(TYPE_CODE_TO_TYPE[enc.srcType], length_output)
     j = 1
-    
+
     for i in 1:2:length(data)
         value = data[i]
         repeat_count = data[i+1]
         output[j:j+repeat_count-1] .= value
         j += repeat_count
     end
-    
+
     return output
 end
 
 # DeltaEncoding
 mutable struct DeltaEncoding <: Encoding
-    srcType::Union{TypeCode, Nothing}
+    srcType::Union{TypeCode,Nothing}
     origin::Int
-    
+
     function DeltaEncoding(; srcType=nothing, origin=0)
         if srcType !== nothing
             srcType = srcType isa TypeCode ? srcType : TYPE_TO_TYPE_CODE[srcType]
@@ -272,7 +276,7 @@ function encode(enc::DeltaEncoding, data)
     if enc.srcType === nothing
         enc.srcType = TYPE_TO_TYPE_CODE[eltype(data)]
     end
-    
+
     data = data .- enc.origin
     diffs = vcat([0], diff(data))
     return convert(Array{Int32}, diffs)
@@ -288,9 +292,9 @@ end
 # IntegerPackingEncoding
 mutable struct IntegerPackingEncoding <: Encoding
     byteCount::Int
-    srcSize::Union{Int, Nothing}
+    srcSize::Union{Int,Nothing}
     isUnsigned::Bool
-    
+
     function IntegerPackingEncoding(byteCount; srcSize=nothing, isUnsigned=false)
         new(byteCount, srcSize, isUnsigned)
     end
@@ -312,12 +316,12 @@ function encode(enc::IntegerPackingEncoding, data)
     elseif enc.srcSize != length(data)
         throw(ArgumentError("Given source size does not match actual data size"))
     end
-    
+
     data = convert(Array{Int32}, data)
     packed_type = determine_packed_dtype(enc)
     min_val = typemin(packed_type)
     max_val = typemax(packed_type)
-    
+
     # Get length of output array by summing up required length of each element
     length_output = 0
     for num in data
@@ -334,11 +338,11 @@ function encode(enc::IntegerPackingEncoding, data)
             length_output += 1
         end
     end
-    
+
     # Fill output
     output = zeros(packed_type, length_output)
     j = 1
-    
+
     for i in 1:length(data)
         remainder = data[i]
         if remainder < 0
@@ -360,7 +364,7 @@ function encode(enc::IntegerPackingEncoding, data)
         output[j] = remainder
         j += 1
     end
-    
+
     return output
 end
 
@@ -368,17 +372,17 @@ function decode(enc::IntegerPackingEncoding, data)
     packed_type = determine_packed_dtype(enc)
     min_val = typemin(packed_type)
     max_val = typemax(packed_type)
-    
+
     # For unsigned integers, do not check lower bound (is always 0)
     # -> Set lower bound to value that is never reached
     if min_val == 0
         min_val = -1
     end
-    
+
     output = zeros(Int32, enc.srcSize)
     j = 1
     unpacked_val = 0
-    
+
     for i in 1:length(data)
         packed_val = data[i]
         if packed_val == max_val || packed_val == min_val
@@ -390,7 +394,7 @@ function decode(enc::IntegerPackingEncoding, data)
             j += 1
         end
     end
-    
+
     return output
 end
 
@@ -400,7 +404,7 @@ mutable struct StringArrayEncoding <: Encoding
     dataEncoding::Vector{Encoding}
     offsetEncoding::Vector{Encoding}
     offsets::Vector{UInt8}
-    
+
     function StringArrayEncoding(; stringData=nothing, dataEncoding=nothing, offsetEncoding=nothing, offsets=nothing)
         if dataEncoding === nothing
             dataEncoding = [ByteArrayEncoding(INT32)]
@@ -416,7 +420,7 @@ function encode(enc::StringArrayEncoding, data)
     if !(eltype(data) <: AbstractString)
         throw(ArgumentError("Data must be of string type"))
     end
-    
+
     if enc.stringData === nothing
         # Get unique stringData
         enc.stringData = unique(data)
@@ -424,11 +428,11 @@ function encode(enc::StringArrayEncoding, data)
     else
         check_present = true
     end
-    
+
     # Sort stringData for binary search
     sorted_indices = sortperm(enc.stringData)
     sorted_strings = enc.stringData[sorted_indices]
-    
+
     # Find indices of each string in data
     indices = zeros(Int32, length(data))
     for i in 1:length(data)
@@ -441,13 +445,13 @@ function encode(enc::StringArrayEncoding, data)
             end
         end
     end
-    
+
     # Apply encodings
     encoded_data = indices
     for encoding in enc.dataEncoding
         encoded_data = encode(encoding, encoded_data)
     end
-    
+
     return encoded_data
 end
 
@@ -455,7 +459,7 @@ function decode(enc::StringArrayEncoding, data)
     # Apply decodings in reverse order
     indices = decode_stepwise(data, enc.dataEncoding) .+ 1
     offsets = decode_stepwise(enc.offsets, enc.offsetEncoding)
-    
+
     substrings = Vector{String}()
 
     # break up the string into the substrings that are individual occurrences
@@ -487,47 +491,47 @@ function deserialize_encoding(content::Any)
     if isa(content, Vector)
         return [deserialize_encoding(item) for item in content]
     end
-    
+
     if isa(content, Encoding)
         return content
     end
     kind = content["kind"]
-    
+
     # if byte convert to integer
     for (key, value) in content
         content[key] = value isa UInt8 ? Int32(value) : value
     end
     params = content
-    
+
     # Handle nested encodings
     if haskey(params, "data_encoding")
         params["data_encoding"] = deserialize_encoding(params["data_encoding"])
     end
-    
+
     if haskey(params, "offsetEncoding")
         params["offsetEncoding"] = deserialize_encoding(params["offsetEncoding"])
     end
-    
+
     encoding_constructors = Dict(
         "ByteArray" => () -> ByteArrayEncoding(INT_TO_TYPE[get(params, "type", nothing)]),
         "FixedPoint" => () -> FixedPointEncoding(params["factor"]; srcType=INT_TO_TYPE[get(params, "srcType", FLOAT32)]),
         "StringArray" => () -> StringArrayEncoding(
-            stringData=get(params, "stringData", nothing), 
+            stringData=get(params, "stringData", nothing),
             dataEncoding=get(params, "dataEncoding", nothing),
             offsetEncoding=get(params, "offsetEncoding", nothing),
             offsets=get(params, "offsets", nothing)
-            ),
-        "IntervalQuantization" => () -> IntervalQuantizationEncoding(params["min"], params["max"], params["numSteps"]; 
-                                                                    srcType=INT_TO_TYPE[get(params, "srcType", 32)]),
-        "RunLength" => () -> RunLengthEncoding(srcSize=get(params, "srcSize", nothing), 
-                                              srcType=INT_TO_TYPE[get(params, "srcType", nothing)]),
-        "Delta" => () -> DeltaEncoding(srcType=INT_TO_TYPE[get(params, "srcType", nothing)], 
-                                      origin=get(params, "origin", 0)),
-        "IntegerPacking" => () -> IntegerPackingEncoding(params["byteCount"], 
-                                                        srcSize=get(params, "srcSize", nothing),
-                                                        isUnsigned=get(params, "isUnsigned", false))
+        ),
+        "IntervalQuantization" => () -> IntervalQuantizationEncoding(params["min"], params["max"], params["numSteps"];
+            srcType=INT_TO_TYPE[get(params, "srcType", 32)]),
+        "RunLength" => () -> RunLengthEncoding(srcSize=get(params, "srcSize", nothing),
+            srcType=INT_TO_TYPE[get(params, "srcType", nothing)]),
+        "Delta" => () -> DeltaEncoding(srcType=INT_TO_TYPE[get(params, "srcType", nothing)],
+            origin=get(params, "origin", 0)),
+        "IntegerPacking" => () -> IntegerPackingEncoding(params["byteCount"],
+            srcSize=get(params, "srcSize", nothing),
+            isUnsigned=get(params, "isUnsigned", false))
     )
-    
+
     if haskey(encoding_constructors, kind)
         return encoding_constructors[kind]()
     else
@@ -539,10 +543,10 @@ end
 function decode_column(column::Dict)
     data = column["data"]
     encodings = []
-    
+
     # Handle the encoding array properly
     for enc in data["encoding"]
-        if  haskey(enc, "dataEncoding")
+        if haskey(enc, "dataEncoding")
             if haskey(enc, "offsetEncoding")
                 push!(encodings, StringArrayEncoding(
                     stringData=enc["stringData"],
@@ -557,7 +561,7 @@ function decode_column(column::Dict)
             push!(encodings, deserialize_encoding(enc))
         end
     end
-    
+
     # Flatten the encodings if needed
     flat_encodings = []
     for enc in encodings
@@ -567,11 +571,11 @@ function decode_column(column::Dict)
             push!(flat_encodings, enc)
         end
     end
-    
+
 
     # return flat_encodings
 
-    decoded =  decode_stepwise(data["data"], flat_encodings)
+    decoded = decode_stepwise(data["data"], flat_encodings)
 
-    
+
 end
